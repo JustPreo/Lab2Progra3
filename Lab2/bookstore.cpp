e569e9a1448dcba354db37cfbe9fbd278ed4f4f8
JustPreo <impre0hn@gmail.com>
2025-10-25 14:48:56 -0600

    a

--- Lab2/bookstore.cpp
+++ Lab2/bookstore.cpp
@@ -24,6 +24,7 @@ int BookStore::searchBookByTitle(string title){
         int mid = low + (high - low)/2;
 
         if (catalogue[mid].title == title){
+            audit.push_front("Search by title: " + title + " FOUND. " + getCurrentTimestamp());
             return mid;
         }
         else if (catalogue[mid].title<title){//Tiene que revisar si el mid es menor , actualiza low
@@ -33,26 +34,29 @@ int BookStore::searchBookByTitle(string title){
             high = mid -1;//Si es mayor , actualiza high
         }
     }
+    audit.push_front("Search by title: " + title + " NOT FOUND. " + getCurrentTimestamp());
     return -1;
 }
 
 int BookStore::searchBookByISBN(string isbn){
-    int low = 0;
-    int high = this->catalogue.size()- 1;
+    std::array<int,13> key = stringToIsbnArray(isbn);
+    std::vector<int> sortedIndices = sortIndicesByIsbn(catalogue);//Convierte array a como deberia de verse
 
-    while (high >= low){
+    int low = 0, high = sortedIndices.size() - 1;
+    while (high >= low) {
         int mid = low + (high - low)/2;
+        const auto& midIsbn = catalogue[sortedIndices[mid]].isbn;
 
-        if (catalogue[mid].isbn == stringToIsbnArray(isbn)){
-            return mid;
-        }
-        else if (catalogue[mid].isbn<stringToIsbnArray(isbn)){//Tiene que revisar si el mid es menor , actualiza low
+        if (midIsbn == key) {
+            audit.push_front("Search by ISBN: " + isbn + " FOUND. " + getCurrentTimestamp());
+            return sortedIndices[mid];
+        } else if (midIsbn < key) {
             low = mid + 1;
-        }
-        else {
-            high = mid -1;//Si es mayor , actualiza high
+        } else {
+            high = mid - 1;
         }
     }
+    audit.push_front("Search by ISBN: " + isbn + " NOT FOUND. " + getCurrentTimestamp());
     return -1;
 }
 //-------------------------Funciones auxiliares-------------------------
@@ -64,10 +68,31 @@ string BookStore::getCurrentTimestamp(){ //Este es el metodo para conseguir el t
 
 }
 
-std::vector<int> BookStore::sortIndicesByIsbn(const std::vector<Book>&){
+std::vector<int> BookStore::sortIndicesByIsbn(const std::vector<Book>& books) {
+    std::vector<int> indices(books.size());//Crea un vector con el tamano de books (que realmente es catalogue)
+    for (int i = 0; i < (int)books.size(); ++i) {
+        indices[i] = i;
+    }
 
+    std::sort(indices.begin(), indices.end(),
+              [&books](int a, int b) {
+                  return books[a].isbn < books[b].isbn;
+              });
+
+    return indices;
 }
 
-std::array<int,13> BookStore::stringToIsbnArray(const std::string& Input){
 
+std::array<int,13> BookStore::stringToIsbnArray(const std::string& Input) {
+    if (Input.size() != 13) //Revisa si el input no tiene el min
+        throw std::invalid_argument("ISBN must have size of 13 ");
+    //Los throws funcionan igualitos a Java , talvez solo cambia eso de std::
+
+    std::array<int,13> arr;
+    for (int i = 0; i < 13; ++i) {
+        if (!isdigit(Input[i]))
+            throw std::invalid_argument("ISBN must contain only numbers");
+        arr[i] = Input[i] - '0'; // convertir char a num
+    }
+    return arr;
 }
--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,8 +1,90 @@
+#include "bookstore.h"
 #include <QCoreApplication>
-
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    BookStore store;
+    string opcion;
+
+    while (true) {
+        cout << "1. Insert Book\n";
+        cout << "2. Search by title\n";
+        cout << "3. Search by ISBN\n";
+        cout << "4. Auditlog\n";
+        cout << "5. Exit\n";
+        cout << "Select an option: ";
+        getline(cin, opcion); //El getline que mencionaba el inge
+
+        if (opcion == "1") {
+            string titulo, autor, isbn;
+            cout << "Title: ";
+            getline(cin, titulo);
+            cout << "Author: ";
+            getline(cin, autor);
+            cout << "ISBN(13 digits): ";
+            getline(cin, isbn);
+
+            try {
+                std::array<int,13> arr = store.stringToIsbnArray(isbn);//Uso la funcion que puso el inge para ocnvertirlo a array
+                Book BOOK(titulo, autor, arr);
+                store.insertBook(BOOK);
+                cout << "Book Added\n";
+            } catch (const std::invalid_argument& e) {
+                cout << e.what() << endl;
+            }
+
+        } else if (opcion == "2") {
+            string titulo;
+            cout << "Name of book to search: ";
+            getline(cin, titulo);
+
+            int pos = store.searchBookByTitle(titulo);
+            if (pos != -1)
+                cout << "Book Found:" << store.catalogue[pos].title << endl;
+            else
+                cout << "Book not found :(";
+
+        } else if (opcion == "3") {
+            string isbn;
+            cout << "ISBN(13 digits): ";
+            getline(cin, isbn);
+
+            try {
+                int pos = store.searchBookByISBN(isbn);
+                if (pos != -1)
+                    cout << "Book Found: " << store.catalogue[pos].title << endl;
+                else
+                    cout << "Book not found\n";
+            } catch (const std::invalid_argument& e) {
+                cout << e.what() << endl;
+            }
+
+        } else if (opcion == "4") {
+            cout << "\n======= AUDIT LOG =======\n";
+            if (store.audit.empty()) {
+                cout << "No logs available\n";
+            } else {
+                for (const string& log : store.audit)//Entendi que el & usa el original y no crea otro en espacio de memoria
+                    cout << log << endl;
+            }
+            cout << "=========================\n";
+
+        } else if (opcion == "5") {
+            cout << "Exiting...\n";
+            break;
+
+        } else {
+            cout << "Invalid Option\n";
+        }
+    }
+    store.catalogue.clear();//Me imagino que esto libera de memoria porque no me deja hacerle delete
+
 
     return a.exec();
 }
